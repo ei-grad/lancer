fb4fc0a51f6c09d1a9f668dc93ab14edd3796ca2
Andrew Grigorev <andrew@ei-grad.ru>
2016-12-18 00:39:13 +0300

    Use errgroup

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,46 +14,36 @@ import (
 	"time"
 )
 
-// C stands for Config or Context
-var C = struct {
-	Low, High *int
-	Duration  *time.Duration
-
-	AccessLogFile, Target, Scheme *string
-}{
-	Low:           flag.Int("l", 1, "RPS value to start test with"),
-	High:          flag.Int("h", 60, "RPS value to finish test with"),
-	Duration:      flag.Duration("d", time.Minute, "test duration"),
-	AccessLogFile: flag.String("f", "access.log", "access.log file location"),
-	Target:        flag.String("t", "localhost", "target"),
-	Scheme:        flag.String("s", "http", "scheme"),
-}
-
 // Parse access.log file, construct http.Request objects and put them to
 // spears channel
-func Parse(filename string, spears chan *http.Request) {
-	f, err := os.Open(*C.AccessLogFile)
+func Parse(ctx context.Context, filename, scheme, target string, spears chan *http.Request) error {
+	f, err := os.Open(filename)
 	if err != nil {
-		log.Printf("Can't open access log file: %s", err)
-		return
+		return err
 	}
 	defer f.Close()
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		line := s.Text()
 		parts := strings.Split(line, " ")
+		// TODO: check method and path validity
 		method := parts[5][1:]
 		path := parts[6]
-		req, err := http.NewRequest(method, fmt.Sprintf("%s://%s%s", *C.Scheme, *C.Target, path), nil)
+		req, err := http.NewRequest(method, fmt.Sprintf("%s://%s%s", scheme, target, path), nil)
 		if err != nil {
 			log.Printf("NewRequest failed: %s", err)
 			continue
 		}
-		spears <- req
+		select {
+		case spears <- req:
+		case <-ctx.Done():
+			return nil
+		}
 	}
 	if s.Err() != nil {
-		log.Printf("Error reading %s: %s", filename, s.Err())
+		return s.Err()
 	}
+	return nil
 }
 
 // Lancer generates linearly increasing load of HTTP requests
@@ -69,18 +61,18 @@ func NewLancer(low, high float64, duration time.Duration) *Lancer {
 	}
 }
 
-// RPSAt calculates an RPS value which should be generated at `t` since start.
+// RPSAt calculates an RPS value at `t`.
 func (l *Lancer) RPSAt(t time.Duration) float64 {
 	return l.low + (l.high-l.low)*float64(t)/float64(l.duration)
 }
 
-// Next calculates the interval to sleep after the request sent at `t` since start.
+// Next calculates the interval between `t` and the next request.
 func (l *Lancer) Next(t time.Duration) time.Duration {
 	return time.Duration(float64(time.Second) / l.RPSAt(t))
 }
 
-// Start to simulate load with ticks sent to lance channel.
-func (l *Lancer) Start(lance chan struct{}) {
+// Lance starts a load simulation with sending ticks to lance channel.
+func (l *Lancer) Lance(lance chan struct{}) {
 	t := time.Duration(0)
 	start := time.Now()
 	finish := start.Add(l.duration)
@@ -96,59 +88,92 @@ func (l *Lancer) Start(lance chan struct{}) {
 	}
 }
 
+// Hit contains info about request timings, sizes and statuses
+// TODO: add ConnectTime, SendTime, ReceiveTime, SizeOut, NetCode
+type Hit struct {
+	Timestamp         time.Time
+	TotalTime         time.Duration
+	SizeIn, ProtoCode int
+	Error             error
+}
+
+// Worker sends an http.Requests coming from spears channel
+func Worker(ctx context.Context, spears chan *http.Request, lance chan struct{}, hits chan Hit) error {
+	for spear := range spears {
+		select {
+		case <-lance:
+		case <-ctx.Done():
+			return nil
+		}
+		// TODO: get rid of creating a goroutine per request, use a dynamic
+		// pool of workers
+		go func(spear *http.Request) {
+			t := time.Now()
+			resp, err := http.DefaultTransport.RoundTrip(spear)
+			if err != nil {
+				hits <- Hit{
+					Timestamp: t,
+					Error:     err,
+				}
+				return
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			// TODO: get additional info from transport layer
+			hits <- Hit{
+				Timestamp: t,
+				ProtoCode: resp.StatusCode,
+				TotalTime: time.Now().Sub(t),
+				SizeIn:    len(body),
+			}
+		}(spear)
+	}
+	return nil
+}
+
 func main() {
+
+	low := flag.Int("l", 1, "RPS value to start test with")
+	high := flag.Int("h", 60, "RPS value to finish test with")
+	duration := flag.Duration("d", time.Minute, "test duration")
+	filename := flag.String("f", "access.log", "access.log file location")
+	target := flag.String("t", "localhost", "target")
+	scheme := flag.String("s", "http", "scheme")
 
 	flag.Parse()
 
-	lancer := NewLancer(float64(*C.Low), float64(*C.High), *C.Duration)
 	spears := make(chan *http.Request)
-
-	go Parse(*C.AccessLogFile, spears)
+	defer close(spears)
 
 	lance := make(chan struct{})
-
-	type Hit map[string]interface{}
+	defer close(lance)
 
 	hits := make(chan Hit)
+	defer close(hits)
 
-	go func() {
-		defer close(hits)
-		for spear := range spears {
-			<-lance
-			go func(spear *http.Request) {
-				tStart := time.Now()
-				resp, err := http.DefaultTransport.RoundTrip(spear)
-				if err != nil {
-					hits <- Hit{
-						"timestamp": tStart,
-						"status":    599,
-						"error":     err.Error(),
-					}
-					return
-				}
-				tHead := time.Now()
-				body, err := ioutil.ReadAll(resp.Body)
-				tAll := time.Now()
-				hits <- Hit{
-					"timestamp":   tStart,
-					"status":      resp.Status,
-					"time_head":   tHead.Sub(tStart),
-					"time_body":   tAll.Sub(tStart),
-					"body_length": len(body),
-				}
-			}(spear)
-		}
-	}()
+	ctx, stop := context.WithCancel(context.Background())
+	g, ctx := errgroup.WithContext(ctx)
 
-	finished := make(chan struct{})
+	g.Go(func() error { return Parse(ctx, *filename, *scheme, *target, spears) })
 
-	go func() {
+	g.Go(func() error { Worker(ctx, spears, lance, hits); return nil })
+
+	g.Go(func() error {
+		// TODO: influxdb output
+		// TODO: phout output
+		// TODO: overload.yandex.ru output
 		for hit := range hits {
 			fmt.Printf("%+v\n", hit)
 		}
-		close(finished)
-	}()
+		return nil
+	})
 
-	lancer.Start(lance)
+	NewLancer(float64(*low), float64(*high), *duration).Lance(lance)
+
+	stop()
+
+	err := g.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
